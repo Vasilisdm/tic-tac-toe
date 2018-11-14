83fb3fd4b8ba0865a8e85a7db9c725623e05e99c
= <=>
2018-11-15 00:03:21 +0200

    Rendered the TicTac component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TicTac from './App';
 import './index.css';
+
+ReactDOM.render(<TicTac />, document.getElementById('root'));
+
+
+  
