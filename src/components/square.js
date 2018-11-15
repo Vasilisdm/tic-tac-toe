533195d27aec8c07a9a7780f21a114a00aee10e8
= <=>
2018-11-16 00:29:11 +0200

    made component stateful,when the user presses a square an x appears

--- src/components/square.js
+++ src/components/square.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 
 class Square extends Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
 }
 
 export default Square;
