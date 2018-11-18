5ba1ae261f45d8ed3d0b894b947bfd8e3b4894ef
= <=>
2018-11-18 17:33:12 +0200

    refactored app file and moved the state in it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Board from './components/board.js';
+import calculateWinner from './components/helpers/calculateWinner.js';
 
 class TicTac extends Component {
 
   constructor(props){
     super(props);
     this.state = {
+      // in order to be able to go in previous move
       history: [{
         squares: Array(9).fill(null)
       }],
@@ -13,10 +15,49 @@ class TicTac extends Component {
     };
   }
 
+  handleClick(i){
+    const history = this.state.history;
+    const current = history[history.length-1];
+    const squares = current.squares.slice();
+    /**
+     * If there is a winner or the square is already written then,
+     * ignore any further click on that particular square.
+     */
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length-1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
     return (
       <div className="tic-tac">
-          <Board squares={this.state.squares}/>
+
+        <section className="game-board">
+          <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
+        </section>
+
+        <section className="game-info">
+          {status}
+        </section>
+
       </div>
     );
   }
