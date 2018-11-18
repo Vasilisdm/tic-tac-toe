4ba331cd34886ec653a6bb59288e3f7aefe56582
= <=>
2018-11-18 16:48:29 +0200

    lifted the state to ancestor component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import Board from './components/board.js';
 
 class TicTac extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    };
+  }
+
   render() {
     return (
       <div className="tic-tac">
-          <Board />
+          <Board squares={this.state.squares}/>
       </div>
     );
   }
