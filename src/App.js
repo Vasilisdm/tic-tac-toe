5eb4f1e0162a43fc18b528cd4acb67e5d57c945d
= <=>
2018-11-18 19:19:25 +0200

    changed history within handleClick()

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class TicTac extends Component {
   }
 
   handleClick(i){
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = history[history.length-1];
     const squares = current.squares.slice();
     /**
